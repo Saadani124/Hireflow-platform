1dc674ec087497068b28eb1530dab970996bbb1a
Saada <talel.saadani.pama@gmail.com>
2026-03-19 16:48:48 +0100

    fixed venv folder + DB enhancements
    libraries updated
    venv malfunctioning
    removed roles table

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ctOS_ERROR\Desktop\TP\s2\Hireflow-platform\venv
+set VIRTUAL_ENV=C:\Users\ctOS_ERROR\Desktop\TP\s2\Hireflow-project\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
